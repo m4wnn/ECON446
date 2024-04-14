6093c7d8c04d8f2d85bf78ea5128192204287178
Mauricio Vargas <38122573+m4wnn@users.noreply.github.com>
2024-04-13 23:02:12 -0700

    Answer to question 1

--- hw/webscraping/main.py
+++ hw/webscraping/main.py
@@ -18,11 +18,13 @@
 # %% 
 import requests
 import string
-import toolz
+import re
 import pandas as pd
+import toolz
 from rich import inspect
 from bs4 import BeautifulSoup
 from tqdm import tqdm
+from multiprocessing import Pool
 # %%
 headers = {
     'Accept-Encoding': 'gzip, deflate, br',
@@ -74,14 +76,91 @@ companies = pd.DataFrame({'company': names, 'ticker': tickers, 'link': links})
 # %%
 #companies.to_csv('data/companies_links.csv', index=False)
 #companies = pd.read_csv('data/companies_links.csv')
+companies = companies.drop_duplicates()
+# %%
+print(companies.head())
 # %% [markdown]
 """
 <!--*!{sections/q01-b.html}-->
 """
+# %%
+def get_type(url):
+    headers = {
+        'Accept-Encoding': 'gzip, deflate, br',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
+    }
+    # Crawling the wiki page of the company. Rise and exception if the url
+    # is broken.
+    try:
+        req = requests.get(url, headers=headers, timeout=20)
+    except:
+        print(f'Request failed in {url}')
+        return ''
+    # Pass if the request is not successful or time out.
+    if not req.ok:
+        return ''
+    soup = BeautifulSoup(req.content, 'html.parser')
+    # Extracting the type of the company from the infobox vcard. Rise an
+    
+    try:
+        vcard = soup.find_all('table', {'class': 'infobox vcard'})[0]
+        comp_type = vcard.find('a', {'title': 'Public company'}).text
+    except:
+        print(f'No company type in {url}')
+        return ''
+    return comp_type
+    
+# %%
+# Multiprocessing to crawl the websites in parallel. 
+with Pool(100) as p:
+    comp_type = p.map(get_type, companies['link'])
+
+# %%
+# Type of companies in lowercase.
+companies['type'] = [x.lower() for x in comp_type]
+# %%
+# Because the format is not uniform between wikis, extract the word 'public'
+# from those companies that have that pattern in their type.
+pattern = re.compile(r'.*public.*')
+companies['type'] = [
+    pattern.sub('public', s) 
+    if pattern.match(s) else s for s in companies.type
+]
+
+# %%
+#companies.to_csv('data/companies_links_with_type.csv', index=False)
+#companies = pd.read_csv('data/companies_links_with_type.csv')
+# %%
+# Companies that are public according to their wiki page.
+print(companies.query("type=='public'"))
 # %% [markdown]
 """
 <!--*!{sections/q01-c.html}-->
 """
+# %%
+companies['clean_ticker'] = [
+    re.match(r"[^,.!? ]+", x).group(0)
+    for x in companies.ticker
+]
+# %%
+companies['len_ticker'] = [len(x) for x in companies.clean_ticker]
+# %%
+len_ticker_pct = toolz.pipe(
+    companies[['clean_ticker', 'len_ticker']],
+    lambda x: x.drop_duplicates(),
+    lambda x: x.groupby('len_ticker'),
+    lambda x: x.count(),
+    lambda x: x * 100 / x.sum()
+)
+# %%
+for i, row in len_ticker_pct.iterrows():
+    tmp = f'''
+    -----------------------------------------
+        Length of the ticker: {i}
+        % in the total of companies: {row.clean_ticker:0.2f}
+    -----------------------------------------
+    '''
+    print(tmp)
 # %% [markdown]
 """
 <!--*!{sections/q02.md}-->
