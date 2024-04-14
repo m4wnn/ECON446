674a7896c8643460e29a567327d8ae09e749b1b1
Mauricio Vargas <38122573+m4wnn@users.noreply.github.com>
2024-04-13 18:02:29 -0700

    Update merged.py

--- hw/webscraping/merged.py
+++ hw/webscraping/merged.py
@@ -36,8 +36,66 @@
 #     <div style="padding: 10px;">
 #         Create a list of links for all the wikipedia pages for NYSE traded companies A-Z and 0-9.
 # </div>
+
 # %%
-URL = "https://en.wikipedia.org/wiki/Companies_listed_on_the_New_York_Stock_Exchange_(A)"
+import requests
+import string
+import toolz
+import pandas as pd
+from rich import inspect
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+# %%
+headers = {
+    'Accept-Encoding': 'gzip, deflate, br',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
+}
+
+# %%
+url = lambda x: f'https://en.wikipedia.org/wiki/Companies_listed_on_the_New_York_Stock_Exchange_({x})'
+
+# %%
+def get_table_info(soup):
+    links = []
+    names = []
+    tickers = []
+
+    # For each row in the table of companies.
+    for r in soup.find_all('tr'):
+        # Get the columns for the row `r`.
+        row = r.find_all('td')
+        # Check if the row contains data.
+        if row:
+            # Extract the link of the company in the row `r`.
+            tmp_link = row[0].find_all('a')
+            # Check if the company has a link.
+            if tmp_link:
+                names.append(tmp_link[0].text)
+                # Adding the root to the relative path.
+                links.append('https://www.wikipedia.org' + tmp_link[0].get('href'))
+                tickers.append(row[1].text.strip())
+    return names, tickers, links        
+
+# %%
+links = []
+names = []
+tickers = []
+for letter in tqdm(list(string.ascii_uppercase) + ['0-9']):
+    req = requests.get(url(letter), headers=headers, timeout=20)
+    # Pass if the request is not successful or time out.
+    if not req.ok:
+        continue
+    soup = BeautifulSoup(req.content, 'html.parser')
+    tmp_n, tmp_t, tmp_l= get_table_info(soup)
+    links.extend(tmp_l)
+    names.extend(tmp_n)
+    tickers.extend(tmp_t)
+
+# %%
+companies = pd.DataFrame({'company': names, 'ticker': tickers, 'link': links})
+# %%
+#companies.to_csv('data/companies_links.csv', index=False)
+#companies = pd.read_csv('data/companies_links.csv')
 # %% [markdown]
 # <div style="border: 1px solid black; border-radius: 5px; overflow: hidden;">
 #     <div style="background-color: black; color: white; padding: 5px; text-align: left;">
